6f7cb70745575368d9c87334f5c50e848410ca9b
ardyutama <ardy.putra@pinhome.id>
2023-09-29 00:35:42 +0700

    feat: remove bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone"
+  }),
   integrations: [tailwind()]
 });
