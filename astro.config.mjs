b06a935b471c79ed67c44817275f3f8466ad7bc2
ardyutama <ardy.putra@pinhome.id>
2023-09-29 00:41:03 +0700

    feat: use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercelStatic from '@astrojs/vercel/static';
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  }),
+    output: 'static',
+  adapter: vercelStatic(),
   integrations: [tailwind()]
 });
