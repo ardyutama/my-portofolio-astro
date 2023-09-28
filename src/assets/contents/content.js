9f95c8b5ea75af609a77fd7fcc9538d1e20aa7c8
ardyutama <ardy.putra@pinhome.id>
2023-09-28 12:08:00 +0700

    feat: prettify, add bullet point, and projects

---
+++ src/assets/contents/content.js
@@ -0,0 +1,26 @@
+let experiences = 
+[
+    {
+        startDate: 'Aug 2022',
+        endDate: 'Feb 2023',
+        position: 'Data Engineer Intern',
+        company: 'PT VKTR Indonesia',
+        skills: ['Python', 'cantools', 'Apache Spark', 'Apache Hadoop', 'Pyspark', 'Cassandra']
+    },
+    {
+        startDate: 'May 2022',
+        endDate: 'Aug 2022',
+        position: 'Frontend Engineer Intern',
+        company: 'Pinhome',
+        skills: ['Jest Unit', 'Javascript', 'ReactJS', 'NextJS', 'Jira', 'Redux.js']
+    },
+    {
+        startDate: 'Jan 2022',
+        endDate: 'Apr 2022',
+        position: 'Full-stack Developer',
+        company: 'Bank UMKM Jawa Timur',
+        skills: ['ReactJS', 'Laravel', 'UI/UX', 'Research UX', 'Figma', 'PHP']
+    }
+];
+
+export {experiences};
