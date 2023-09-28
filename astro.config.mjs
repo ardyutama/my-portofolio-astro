58879e67a294ac8b03d1b33809cc017a5ba703e2
ardyutama <ardy.putra@pinhome.id>
2023-09-29 00:06:29 +0700

    feat: add node by bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import nodejs from '@astrojs/node'
-
 import react from '@astrojs/react';
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	output:'hybrid',
-	adapter: nodejs({
-		mode: 'middleware'
-	}),
-	integrations: [tailwind(), react()],
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [tailwind(), react()]
 });
